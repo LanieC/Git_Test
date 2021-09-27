5e72387ab342bd0e67e2b574f1bb679cfacc57e5
LanieC <91410696+LanieC@users.noreply.github.com>
2021-09-27 19:40:15 +0800

    pull commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("master hello git4!");
         System.out.println("hot-fix hello git4!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
